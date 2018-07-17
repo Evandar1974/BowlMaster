13935e83d85ee72e98ffada838225c91f0880b61
Mark <markjcarroll@hotmail.co.uk>
2018-07-17 10:33:57 +0100

    Added chase camera script.

---
+++ BowlMaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // Use this for initialization
+    public float launchSpeed;
+
+    private Rigidbody body;
+    private AudioSource audioSource;
+	void Start ()
+    {
+        body = this.GetComponent<Rigidbody>();
+        audioSource = this.GetComponent<AudioSource>();
+
+        Launch();
+
+    }
+
+    private void Launch()
+    {
+        body.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
