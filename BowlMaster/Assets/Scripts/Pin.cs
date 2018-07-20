6904202503ef4ceaf11a1c0e17f0a3990a23d997
Mark <markjcarroll@hotmail.co.uk>
2018-07-20 22:46:17 +0100

    pin counter refined and count added to ui element

--- BowlMaster/Assets/Scripts/Pin.cs
+++ BowlMaster/Assets/Scripts/Pin.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Pin : MonoBehaviour
 {
-    public float standingThreshold = 20;
+    public float standingThreshold = 5f;
     private Vector3 rotation;
 	// Use this for initialization
 	void Start ()
     {
-        IsStanding();
+       
 	}
 	
 	// Update is called once per frame
@@ -22,7 +22,6 @@ public class Pin : MonoBehaviour
         rotation = this.transform.rotation.eulerAngles;
         float tiltX = Mathf.Abs(rotation.x - 270f);
         float tiltZ = Mathf.Abs(rotation.z);
-        print(tiltX + " " + tiltZ);
         return (tiltX < standingThreshold && tiltZ < standingThreshold);
     }
 }
