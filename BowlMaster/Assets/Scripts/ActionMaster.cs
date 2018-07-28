59b8dbcbe6c35a9480fadadec1dad20c97d76afa
Mark <markjcarroll@hotmail.co.uk>
2018-07-28 14:47:36 +0100

    Actionmaster cs added with TDD actionMasterTest.cs continuing to build with TDD principles

---
+++ BowlMaster/Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ActionMaster
+{
+    public enum Action { Tidy, Reset, EndTurn, EndGame };
+    private int[] bowls = new int[21];
+    private int bowl = 1;
+    public Action Bowl(int pins)
+    {
+        if (pins > 10 || pins < 0)
+        {
+            throw new UnityException("Illegal number Of Pins");
+        }
+        if(pins == 10)
+        {
+            bowl += 2;
+            return Action.EndTurn;
+
+        }
+        if(bowl % 2 != 0) 
+        {
+            bowl += 1;
+            return Action.Tidy;
+        }
+        throw new UnityException("Not sure what to do");
+    }
+}
+ 
---
+++ BowlMaster/Assets/Scripts/Tests/ActionMasterTests.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class ActionMasterTests
+{
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+    private ActionMaster actionMaster = new ActionMaster();
+    [Test]
+    public void T00PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsendTurn()
+    {
+        
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+
+
+    
+ 
+}
