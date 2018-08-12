7e7df9f92cdde59a5cea6edd1c3db21bf4596632
Mark <markjcarroll@hotmail.co.uk>
2018-08-12 22:45:07 +0100

    added further tests

--- BowlMaster/Assets/Scripts/Tests/ScoreMasterTest.cs
+++ BowlMaster/Assets/Scripts/Tests/ScoreMasterTest.cs
@@ -53,5 +53,106 @@ public class ScoreMasterTest
         results.Add(30);
         Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
     }
+    [Test]
+    public void T06FourBallsNoStrikesTwoResults()
+    {
+        int[] balls = [3, 5, 6, 2];
+        int[] frames = [8, 8];
+        foreach(int i in balls)
+        {
+            pinDrops.Add(i);
+        }
+        foreach(int i in frames)
+        {
+            results.Add(i);
 
+        }
+        Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
+
+    }
+    [Test]
+    public void T07FullGameNoStrikesOrSpares()
+    {
+        int[] balls = [3, 5, 6, 2, 5, 3, 6, 1, 8, 0, 7, 2, 6, 3, 3, 5, 4, 5, 4, 5];
+        int[] frames = [8, 8, 8, 7, 8, 9, 9, 8, 9, 9];
+        foreach (int i in balls)
+        {
+            pinDrops.Add(i);
+        }
+        foreach (int i in frames)
+        {
+            results.Add(i);
+
+        }
+        Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
+
+    }
+    [Test]
+    public void T08FourBallsNoStrikesTwoResults()
+    {
+        int[] balls = [3, 5, 6, 2];
+        int[] frames = [8, 8];
+        foreach (int i in balls)
+        {
+            pinDrops.Add(i);
+        }
+        foreach (int i in frames)
+        {
+            results.Add(i);
+
+        }
+        Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
+
+    }
+    [Test]
+    public void T09FourBallsNoStrikesTwoResults()
+    {
+        int[] balls = [3, 5, 6, 2];
+        int[] frames = [8, 8];
+        foreach (int i in balls)
+        {
+            pinDrops.Add(i);
+        }
+        foreach (int i in frames)
+        {
+            results.Add(i);
+
+        }
+        Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
+
+    }
+    [Test]
+    public void T10FourBallsNoStrikesTwoResults()
+    {
+        int[] balls = [3, 5, 6, 2];
+        int[] frames = [8, 8];
+        foreach (int i in balls)
+        {
+            pinDrops.Add(i);
+        }
+        foreach (int i in frames)
+        {
+            results.Add(i);
+
+        }
+        Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
+
+    }
+    [Test]
+    public void T11FourBallsNoStrikesTwoResults()
+    {
+        int[] balls = [3, 5, 6, 2];
+        int[] frames = [8, 8];
+        foreach (int i in balls)
+        {
+            pinDrops.Add(i);
+        }
+        foreach (int i in frames)
+        {
+            results.Add(i);
+
+        }
+        Assert.AreEqual(results, scoreMaster.ScoreFrames(pinDrops));
+
+    }
 }
